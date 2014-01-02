207fdd8b872d8f51f9f992136ea3c373f2af8bde
Cassidy Williams <cassidoo@outlook.com>
2014-01-01 22:40:59 -0600

    Beauty and the Beast -"and the Beast"

--- texttoarray.py
+++ texttoarray.py
@@ -5,12 +5,14 @@ import urllib
 input = open('input.txt','r') # file from which you're reading
 output = open("output.js", "w") # file to which you'll write
 
-output.write("var array = [") # start table tag
+output.write("var array = [") # start array tag
 
+out_text = "" 
 for line in input:
-	output.write(line + ", ") # writing the list to array
-	
-output.write("];") # end table tag
+	out_text += "'" + line.strip("\n") + "', "
+output.write(out_text[:-2]) # this takes out the last comma and space, thank you @ingridavendano
+
+output.write("];") # end array tag
 
 input.close()
 output.close()
